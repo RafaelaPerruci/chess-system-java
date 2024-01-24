1cba28f2bb3af164ffbef952c574d2dcec301460
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-01-24 15:31:41 -0300

    Arquivo README adicionado e primeira classe Position

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chess system
+Projeto construído com o objetivo de praticar os conhecimentos adquiridos em Java até o momento.
+
+*Curso Nélio Alves - Plataforma Udemy*
---
+++ src/br/com/rafaelaperruci/application/Main.java
@@ -0,0 +1,16 @@
+package br.com.rafaelaperruci.application;
+
+import br.com.rafaelaperruci.boardgame.Position;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Position pos1 = new Position(3, 5);
+        System.out.println(pos1);
+
+
+    }
+}
