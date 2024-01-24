7d8b51cf9b784d777752e585768f0240d9610a5f
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-01-24 19:29:05 -0300

    Classe de exceção adicionada e devidas verificações

--- src/br/com/rafaelaperruci/boardgame/Board.java
+++ src/br/com/rafaelaperruci/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if ( rows < 1 || columns < 1){
+            throw new BoardException("Error: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -16,25 +19,42 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
     public Piece piece(int row, int column){
-        return pieces [row] [column];
+        if (!positionExists(row, column)){
+            throw new BoardException("Error: Position not on the board.");
+        }
+
+        return pieces [row] [column];     //método que retorna a peça correspondente a linha e coluna da matriz
     }
     public Piece piece(Position position){
-        return pieces[position.getRow()] [position.getColumn()];
+        if (!positionExists(position)){
+            throw new BoardException("Error: Position not on the board.");
+        }
+        return pieces[position.getRow()] [position.getColumn()]; //aqui retorna a exata posição em que a peça está
     }
     public void placePiece(Piece piece, Position position){
-        pieces[position.getRow()][position.getColumn()] = piece;
-        piece.position = position;
+        if (thereIsAPiece(position)){
+            throw new BoardException("Error, there is already a piece on position: " + position);
+        }
+
+        pieces[position.getRow()][position.getColumn()] = piece;    //aqui eu atribuo a posição da matriz a peça correspondente
+        piece.position = position;                                 //e aqui eu atualizo sua posição
+    }
+
+    private boolean positionExists(int row, int column){
+        return row >=0 && row < rows && column >=0 && column < columns;
+    }
+    public boolean positionExists(Position position){
+        return  positionExists(position.getRow(), position.getColumn());
+    }
+    public boolean thereIsAPiece(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Error: Position not on the board.");
+        }
+        return piece(position) != null;               //se o método chamado retornar uma posição, indica que ele é != null
     }
 }
