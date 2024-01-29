3bc30be6133b9f3dab886b866ee5231237161464
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-01-29 16:03:50 -0300

    checkmate implementado

--- src/br/com/rafaelaperruci/application/Main.java
+++ src/br/com/rafaelaperruci/application/Main.java
@@ -19,7 +19,7 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces());
@@ -51,5 +51,7 @@ public class Main {
                 System.out.println(e.getMessage());
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
